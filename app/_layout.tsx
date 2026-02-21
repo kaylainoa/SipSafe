4dd0cdacbc9a33f5620d00681690fa3dba0d1b9a
Kayla Inoa <kinoa718@gmail.com>
2026-02-20 21:06:28 -0500

    Floating Action button to log drinks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+// app/_layout.tsx
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import DrinkTrackerFAB from '@/components/DrinkTrackerFAB';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,10 +17,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      <DrinkTrackerFAB>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </DrinkTrackerFAB>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
